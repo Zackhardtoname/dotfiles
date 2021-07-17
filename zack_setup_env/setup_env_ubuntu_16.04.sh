597cddaeef8917d5c16e85a650a65f7e40133e7c
Zack Light <zack.hardtoname@gmail.com>
2021-07-17 15:20:13 -0700

    Update zack_setup_env/setup_env_ubuntu_16.04.sh

--- zack_setup_env/setup_env_ubuntu_16.04.sh
+++ zack_setup_env/setup_env_ubuntu_16.04.sh
@@ -47,6 +47,12 @@ git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
 p10k configure
 
+#github cli
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo gpg --dearmor -o /usr/share/keyrings/githubcli-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+sudo apt update
+sudo apt install gh
+
 # chezmoi
 curl -sfL https://git.io/chezmoi | sh
 
