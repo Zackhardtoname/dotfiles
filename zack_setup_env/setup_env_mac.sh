50d33bb71e2460d9c5aa0a0acd3ae5adbed13b15
Zack Light <zack.hardtoname@gmail.com>
2022-01-29 22:39:23 -0800

    Add zack_setup_env/setup_env_mac.sh

---
+++ zack_setup_env/setup_env_mac.sh
@@ -0,0 +1,26 @@
+# create ~/Library/KeyBindings/ and save a property list like this as DefaultKeyBinding.dict
+# {
+#    /* Remap Home / End keys */
+#    /* Home Button*/
+#    "\UF729" = "moveToBeginningOfLine:";
+#    /* End Button */
+#    "\UF72B" = "moveToEndOfLine:";
+#    /* Shift + Home Button */
+#    "$\UF729" = "moveToBeginningOfLineAndModifySelection:";
+#    /* Shift + End Button */
+#    "$\UF72B" = "moveToEndOfLineAndModifySelection:";
+#    /* Ctrl + Home Button */
+#    "^\UF729" = "moveToBeginningOfDocument:";
+#    /* Ctrl + End Button */
+#    "^\UF72B" = "moveToEndOfDocument:";
+#    /* Shift + Ctrl + Home Button */
+#    "$^\UF729" = "moveToBeginningOfDocumentAndModifySelection:";
+#    /* Shift + Ctrl + End Button*/
+#    "$^\UF72B" = "moveToEndOfDocumentAndModifySelection:";
+# 
+#    "^\UF702" = moveWordLeft:;
+#    "^\UF703" = moveWordRight:;
+#    "^$\UF702" = moveWordLeftAndModifySelection:;
+#    "^$\UF703" = moveWordRightAndModifySelection:;
+#  }
+ 
